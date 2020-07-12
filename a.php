8057abb6f564222982022c0d2ea89d0b980e1f1f
Khoa <khoa@Khoas-iMac.local>
2020-07-12 16:40:25 +0700

    change to php

---
+++ a.php
@@ -0,0 +1,20 @@
+Website AAA.com <br \>
+Hello <?php //echo $_SERVER['REMOTE_ADDR']; ?>
+<?php
+//whether ip is from share internet
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+  {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+  }
+//whether ip is from proxy
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+  {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+//whether ip is from remote address
+else
+  {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+  }
+echo $ip_address;
+?>
